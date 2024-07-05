b1e2cbdd239bd6ae9e6c5d59d78fa6f2b53c94e0
VyacheslavDotsenko <dotsenko.01@list.ru>
2024-07-05 17:43:08 +0300

    create postgres_dag.py

---
+++ dags/postgres_dag.py
@@ -0,0 +1,20 @@
+import datetime as dt
+
+from airflow import DAG
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+default_args = {
+    'owner': 'Vyacheslav',
+    'start_date': dt.datetime(2024, 7, 1),
+    'retries': 2,
+}
+
+with DAG(dag_id='postgres_dag',  # уникальный id
+         schedule_interval='@once',  # расписание запусков
+         default_args=default_args) as dag:
+    test_sql = PostgresOperator(
+        task_id='select_limit_1',
+        postgres_conn_id='postgres_default',
+        sql="SELECT * FROM source.source_data.базы_данных LIMIT 1",
+        dag=dag,
+    )
